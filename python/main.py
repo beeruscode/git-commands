b26ee7c2dc46543b9e6f5b9a187b4b4da6c27b34
mahboob <mahboob.com>
2026-09-09 10:26:20 -0700

    01

---
+++ python/main.py
@@ -0,0 +1,10 @@
+#take two number from user and do sum diff and division and 
+a  = int(input ("Enter first numbers to do operations "))
+b = int(input ("Enter second numbers to do operations "))
+
+
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
